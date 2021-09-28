a8e8659f31c691490badd41220e785f1d770cb47
JiangLiWei <280571835@qq.com>
2021-09-28 14:37:40 +0800

    test

---
+++ spring-mybatis-02/src/main/java/com/jlw/App.java
@@ -0,0 +1,13 @@
+package com.jlw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
